2c3f19c1f8049a75a32a8ffe553fd4ae67f78832
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 08:52:48 -0500

    create student class in kotlin

---
+++ clase_4.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val id: Long = 123456789L
+    val name: String = "Mateo"
+    val lastname: String = "Salazar"
+    val email: String = "mateosalazar@hotmail.com"
+
+    println("ID: $id")
+    println("Hi, $name $lastname!")
+    println("Email: $email")
+}
